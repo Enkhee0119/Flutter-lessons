b423cd1b73730d5ed725d2fa54248261b137b08e
Enkhee <enheeenhee253@gmail.com>
2022-12-27 20:33:57 +0800

    movie detail page hiih

--- lib/screens/movieDetail.dart
+++ lib/screens/movieDetail.dart
@@ -1,12 +1,103 @@
 import 'package:flutter/material.dart';
+import 'package:movie_app/main.dart';
+import 'package:movie_app/model/movie/index.dart';
 
 class MovieDetailPage extends StatelessWidget {
-  const MovieDetailPage({super.key});
+  final MovieModel data;
+  const MovieDetailPage(this.data, {super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Movie detail page"),
+    final width = MediaQuery.of(context).size.width;
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 24, 22, 22),
+      body: CustomScrollView(slivers: [
+        // SliverAppBar(
+        //   title: Text(data.title),
+        // ),
+        SliverList(
+            delegate: SliverChildListDelegate(
+          [
+            SizedBox(
+              width: width,
+              height: width * 1.2,
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  Image.network(
+                    data.imgUrl,
+                    width: width,
+                    fit: BoxFit.fitWidth,
+                  ),
+                  Container(
+                    color: Colors.black.withOpacity(0.5),
+                  ),
+                  Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Icon(
+                          Icons.play_circle,
+                          color: Colors.white.withOpacity(0.3),
+                          size: 50,
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          data.title ?? "",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 20),
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          "${data.publishedYear}|${data.durationMin}|${data.type}",
+                          style: TextStyle(color: Color(0xffcccccc)),
+                        ),
+                        SizedBox(height: 50)
+                      ],
+                    ),
+                  ),
+                  SafeArea(
+                      child: Align(
+                    alignment: Alignment.topLeft,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.chevron_left_rounded,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ))
+                ],
+              ),
+            ),
+            Column(
+              children: [
+                SizedBox(height: 20),
+                Text(
+                  "Тайлбар",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20),
+                ),
+                SizedBox(height: 20),
+                Text(
+                  data.description ?? "",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xffbbbbbb),
+                  ),
+                ),
+                SizedBox(height: 30)
+              ],
+            )
+          ],
+        ))
+      ]),
     );
   }
 }
--- lib/widgets/movie_special_card.dart
+++ lib/widgets/movie_special_card.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:movie_app/model/movie/index.dart';
+import 'package:movie_app/screens/movieDetail.dart';
 
 class MovieSpecialCard extends StatelessWidget {
   final MovieModel data;
   const MovieSpecialCard(this.data, {super.key});
+  void _onCardTap(BuildContext context) {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (_) => MovieDetailPage(data)));
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.width * 0.6,
-      width: size.width * 0.4,
-      margin: EdgeInsets.only(right: 10),
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          image: DecorationImage(
-              image: NetworkImage(data.imgUrl), fit: BoxFit.fill)),
-      child: Icon(
-        Icons.play_circle,
-        color: Colors.grey.withOpacity(0.5),
-        size: 60,
-      ),
-    );
+    return InkWell(
+        onTap: () => _onCardTap(context),
+        child: Container(
+          height: size.width * 0.6,
+          width: size.width * 0.4,
+          margin: EdgeInsets.only(right: 10),
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              image: DecorationImage(
+                  image: NetworkImage(data.imgUrl), fit: BoxFit.fill)),
+          child: Icon(
+            Icons.play_circle,
+            color: Colors.grey.withOpacity(0.5),
+            size: 60,
+          ),
+        ));
   }
 }
